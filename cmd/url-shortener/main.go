8743ded6a99f141d2c99b83d4c7a5f5f4c42dc38
Kerlenton <semyonschv@yandex.ru>
2025-03-06 18:56:08 +0300

    init config package

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	// TODO: init config: cleanevn
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init server: chi, "chi render"
+
+	// TODO: run server
+}
